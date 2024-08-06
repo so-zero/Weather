88371b7bd471c9c2e2d81e09b48f61ad59641069
so-zero <zero_s0@naver.com>
2024-08-06 18:27:08 +0900

    icons

---
+++ app/utils/Icons.tsx
@@ -0,0 +1,39 @@
+import {
+  Command,
+  Search,
+  Navigation,
+  Gauge,
+  Eye,
+  UserRound,
+  CalendarDays,
+  Cloudy,
+  CloudDrizzle,
+  CloudRain,
+  CloudSun,
+  Thermometer,
+  ThermometerSun,
+  Wind,
+  Sunset,
+  SunDim,
+  Droplets,
+  Snowflake,
+} from "lucide-react";
+
+export const command = <Command size={14} />;
+export const search = <Search />;
+export const navigation = <Navigation size={15} />;
+export const gauge = <Gauge size={15} />;
+export const eye = <Eye size={15} />;
+export const userIcon = <UserRound size={15} />;
+export const calendar = <CalendarDays size={15} />;
+export const cloudy = <Cloudy size={30} />;
+export const drizzle = <CloudDrizzle size={25} />;
+export const rain = <CloudRain size={30} />;
+export const cloudSun = <CloudSun size={30} />;
+export const thermometer = <Thermometer size={15} />;
+export const thermometerSun = <ThermometerSun size={15} />;
+export const wind = <Wind size={15} />;
+export const sunset = <Sunset size={15} />;
+export const sunDim = <SunDim size={15} />;
+export const droplets = <Droplets size={15} />;
+export const snow = <Snowflake size={30} />;
