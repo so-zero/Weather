8d5854fb937d28d4e04740d44bd1581b7b1011b7
so-zero <zero_s0@naver.com>
2024-08-10 14:35:54 +0900

    default states

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,34 @@
+const defaultStates = [
+  {
+    name: "Seoul",
+    country: "KR",
+    lat: 37.568,
+    lon: 126.978,
+  },
+  {
+    name: "Tokyo",
+    country: "JP",
+    lat: 35.69,
+    lon: 139.692,
+  },
+  {
+    name: "Beijing",
+    country: "CN",
+    lat: 39.907,
+    lon: 116.397,
+  },
+  {
+    name: "Vietnam",
+    country: "VN",
+    lat: 16.167,
+    lon: 107.833,
+  },
+  {
+    name: "Thailand",
+    country: "TH",
+    lat: 15.0,
+    lon: 100.0,
+  },
+];
+
+export default defaultStates;
